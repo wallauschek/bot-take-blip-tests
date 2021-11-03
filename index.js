d4f72a38eba7698976a7334ff9268d4480a71c8f
Pedro Wallauschek <pedro@pedrowallauschek.com.br>
2021-11-03 07:23:56 -0300

    verificando conversação

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const saudacoes = [
+  "Oi",
+  "Olá",
+  "oi",
+  "olá",
+  "ola",
+  "hello",
+  "tudo bom",
+  "iai",
+  "eai",
+];
+
 // Put your identifier and access key here
 let IDENTIFIER = process.env.BLIP_IDENTIFIER;
 let ACCESS_KEY = process.env.BLIP_ACCESS_KEY;
@@ -15,24 +27,161 @@ let client = new BlipSdk.ClientBuilder()
   .withTransportFactory(() => new WebSocketTransport())
   .build();
 
-// Connect with server asynchronously
-// Connection will occurr via websocket on 8081 port.
 client
-  .connect() // This method return a 'promise'.
-  .then(function (session) {
-    // Connection success. Now is possible send and receive envelopes from server. */
-    console.log("Application started. Press Ctrl + c to stop.");
+  .connect()
+  .then(async function () {
+    console.log("Bot Connectado - pesquisa satisfação");
+
+    client.addMessageReceiver(
+      function (message) {
+        if (saudacoes.indexOf(message.content) != -1) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+      function (message) {
+        //send a message to some user
+        var msg = {
+          type: "text/plain",
+          content:
+            "Bem Vindo, gostaria de realizar a nossa pesquisa? responda com sim ou não",
+          to: message.from,
+        };
+        client.sendMessage(msg);
+        //######################## Primeira Chave da pesquisa
+        client.addMessageReceiver(
+          function (message) {
+            switch (message.content) {
+              case "sim":
+                return true;
+                break;
+              case "não":
+                var msg = {
+                  type: "text/plain",
+                  content:
+                    "Tudo Bem! nos encontramos numa próxima vez então! obrigado! até mais!",
+                  to: message.from,
+                };
+                client.sendMessage(msg);
+                return false;
+                break;
+              default:
+                return false;
+                break;
+            }
+          },
+          function (message) {
+            //send a message to some user
+            var msg = {
+              type: "text/plain",
+              content:
+                "Okay, qual tipo de jogo você mais joga? responda com o número da sua opção: \n" +
+                "1-RPG \n2-FPS/TPS \n3-Estratégia \n4-Indie \n5-Esportes 6-Nenhum",
+              to: message.from,
+            };
+
+            client.sendMessage(msg);
+
+            client.addMessageReceiver(
+              function (message) {
+                if (message.content == 1) {
+                  client.addMessageReceiver(true, async function (message) {
+                    //send a message to some user
+                    var msg = {
+                      type: "text/plain",
+                      content:
+                        "Então você é um aventureiro! obrigado por participar de nossa pesquisa!",
+                      to: message.from,
+                    };
+
+                    client.sendMessage(msg);
+                    message.content = "";
+                    // sleep(2);
+                  });
+                } else if (message.content == 2) {
+                  client.addMessageReceiver(true, function (message) {
+                    //send a message to some user
+                    var msg = {
+                      type: "text/plain",
+                      content:
+                        "Soldado! vê se não sai atirando em civis por aí! obrigado por participar de nossa pesquisa!",
+                      to: message.from,
+                    };
+                    client.sendMessage(msg);
+                    // sleep(2);
+                  });
+                } else if (message.content == 3) {
+                  client.addMessageReceiver(true, function (message) {
+                    //send a message to some user
+                    var msg = {
+                      type: "text/plain",
+                      content:
+                        "um intelectual e calculista! obrigado por participar de nossa pesquisa!",
+                      to: message.from,
+                    };
+                    client.sendMessage(msg);
+                    // sleep(2);
+                  });
+                } else if (message.content == 4) {
+                  client.addMessageReceiver(true, function (message) {
+                    //send a message to some user
+                    var msg = {
+                      type: "text/plain",
+                      content:
+                        "Haha um Indiano! se divirta sem compromisso! obrigado por participar de nossa pesquisa!",
+                      to: message.from,
+                    };
+                    client.sendMessage(msg);
+                    // sleep(2);
+                  });
+                } else if (message.content == 5) {
+                  client.addMessageReceiver(true, function (message) {
+                    //send a message to some user
+                    var msg = {
+                      type: "text/plain",
+                      content:
+                        "Se treinar vai ser o melhor jogador! obrigado por participar da nossa pesquisa!",
+                      to: message.from,
+                    };
+                    client.sendMessage(msg);
+                    // sleep(2);
+                  });
+                } else if (message.content == 6) {
+                  return true;
+                } else {
+                  return false;
+                }
+              },
+              function (message) {
+                //send a message to some user
+                var msg = {
+                  type: "text/plain",
+                  content:
+                    "Bom! nem todos se encaixam nesse contexto! mas obrigado por participar da pesquisa",
+                  to: message.from,
+                };
+                client.sendMessage(msg);
+                // sleep(2);
+              }
+            );
+          }
+        );
+        //########################################1
+      }
+    );
+
+    //### Connection
   })
   .catch(function (err) {
-    /* Connection failed. */
+    console.log(err);
   });
 
-client.addMessageReceiver("text/plain", async function (message) {
-  var msg = {
-    type: "text/plain",
-    content: `${message.content} - retorno`,
-    to: message.from,
-  };
-  console.log(typeof message.content != "object" ? message : "");
-  typeof message.content != "object" ? client.sendMessage(msg) : "";
-});
+async function sleep(seconds) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  // Sleep in loop
+  for (let i = 1; i <= seconds; i++) {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
+  process.exit(0);
+}
